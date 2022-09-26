22f988794e1ee5fde653764e4fbb08afc0ea49cf
Charitha <Charitha.Neelisetty@ivycomptech.com>
2022-09-26 20:07:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sept26_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("C:\\Users\\Charitha.Neelisetty\\OneDrive - Entain Group\\Documents\\sample.txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("This example adds content to existing file");
+            Console.WriteLine("Text appended successfully!!!");
+            sw.Close();
+            fs.Close();
+
+
+        }
+    }
+}
